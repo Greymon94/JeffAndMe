fa220519e683094d6eb27071871323376b0c2325
Losapioj <Losapioj@gmail.com>
2016-03-02 11:28:17 -0500

    Basic Main Menu Implemented

---
+++ New Unity Project/Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour 
+{
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void NewGame()
+    {
+        Application.LoadLevel(1);
+    }
+    public void LoadGame()
+    {
+        Debug.Log("Not Yet Implemented");
+    }
+    public void Credits()
+    {
+        Application.LoadLevel("Credits");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

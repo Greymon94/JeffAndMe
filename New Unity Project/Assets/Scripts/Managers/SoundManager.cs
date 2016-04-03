f1673831a734695abdf5efa3c36c4f10838b65c3
Losapioj <Losapioj@gmail.com>
2016-04-03 19:50:08 -0400

    sound management work still not working, unsure why

---
+++ New Unity Project/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+
+    /*
+    private static SoundManager _instance;
+    public static SoundManager Instance
+    {
+        set
+        {
+            _instance = value;
+        }
+        get
+        {
+            if (!_instance)
+                new GameObject().AddComponent<SoundManager>();
+            return _instance;
+        }
+    }
+
+    public List<AudioClip> audioClips = new List<AudioClip>();
+
+    private Dictionary<string, AudioClip> clipDict = new Dictionary<string,AudioClip>;
+
+    private List<AudioSource> playing;
+
+    void Start()
+    {
+        foreach(AudioClip a in audioClips)
+        {
+            clipDict.Add(a.name, a);
+        }
+    }
+
+    public bool PlaySoundOnce(string name, AudioSource soundSource, float volume)
+    {
+        if (clipDict.Count == 0 || !clipDict.ContainsKey(name))
+            return false;
+
+        soundSource.PlayOneShot(clipDict[name], volume);
+        return true;
+    }
+    //*/
+}
---
+++ New Unity Project/Assets/Scripts/Sound Events/PlaySound.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider))]
+
+public class PlaySound : MonoBehaviour 
+{
+    public AudioClip soundClip;
+    AudioSource myAudio;
+
+    public enum PlayStart {onEnter, onExit};
+    public PlayStart whenToStart;
+
+    public enum PlayEnd { endOfClip, onEnter, onExit, onTimer };
+    public PlayEnd whenToEnd;
+
+    public float optionalTimer;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+        myAudio.clip = soundClip;
+        GetComponent<Collider>().isTrigger = true;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGER ENTERED");
+        if (other.tag != "Player")
+        {
+            Debug.Log("Collided with not player");
+            return;
+        }
+
+        if (whenToStart == PlayStart.onEnter)
+        {
+            Debug.Log("starting audio");
+            //other.GetComponent<AudioSource>().PlayOneShot(soundClip);
+            StartClip(optionalTimer, other);
+        }
+        else if (whenToEnd == PlayEnd.onEnter)
+        {
+            StopClip();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            Debug.Log("Collided with not player");
+            return;
+        }
+
+        if (whenToStart == PlayStart.onExit)
+        {
+            StartClip(optionalTimer, other);
+        }
+        else if (whenToEnd == PlayEnd.onExit)
+        {
+            StopClip();
+        }
+    }
+
+    IEnumerator StartClip(float timer, Collider c)
+    {
+        /*
+        myAudio = null;
+        foreach (AudioSource a in c.GetComponents<AudioSource>())
+        {
+            if (!a.isPlaying)
+            {
+                myAudio = a;
+                break;
+            }
+        }
+        if (myAudio == null)
+        {
+            myAudio = c.gameObject.AddComponent<AudioSource>();
+        }
+        myAudio.clip = soundClip;
+        //*/
+        myAudio.Stop();
+        myAudio.Play();
+        if (timer > 0)
+        {
+            yield return new WaitForSeconds(timer);
+            myAudio.Stop();
+        }
+    }
+
+    void StopClip()
+    {
+        myAudio.Stop();
+    }
+}
